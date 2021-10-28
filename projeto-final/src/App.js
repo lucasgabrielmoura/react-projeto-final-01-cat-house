a84c8c29c5f3d3a4c0df1ea115d9d2265c1b0b34
lucasgabrielmoura <lucasgabrieldemoura.2021@gmail.com>
2021-10-28 10:09:10 -0300

    base-project

--- projeto-final/src/App.js
+++ projeto-final/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Galeria from "./components/galeria/Galeria";
-import {BrowserRouter} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Galeria />
-      </div>
-    </BrowserRouter>
+        <>
+          <Galeria />
+        </>
   );
 }
 
