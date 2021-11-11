05f3d5073637e6abb57c2805d7d1bd5862c5149e
Henrique <henriquefernandes.gouveia@gmail.com>
2021-11-11 01:43:13 -0300

    adicionando loading

--- projeto-final/src/components/galeria/Galeria.js
+++ projeto-final/src/components/galeria/Galeria.js
@@ -6,10 +6,15 @@ import Footer from '../Footer/Footer';
 import arrow from './img/arrow-down.svg';
 import bota from './img/4.png';
 import {Link} from 'react-router-dom';
+import Load from "./img/load.png";
 
 /*https://api.thecatapi.com/v1/images/search?format=json&limit=25*/
 
 export default function Galeria({setDados}) {
+
+
+    const [load, setLoad] = useState(false)
+
 
     const [imagens, setImagem] = useState([])
     function puxarDado(imagem){
@@ -18,16 +23,20 @@ export default function Galeria({setDados}) {
       })  
       console.log(imagem)
     }
-
-    useEffect(() => {
+    function loading(){
         Imageapi
             .get(`?mime_types=jpg&size=full&has_breeds=1&limit=25`)
             .then((res) => {
                 console.log(res.data)
                 setImagem(res.data)
+                setLoad(true)
 
             })
             .catch((err) => console.error("ops! ocorreu um erro" + err))
+    }
+
+    useEffect(() => {
+        loading()
     }, [])
     useEffect(() => {
         document.title = "Galeria"
@@ -53,7 +62,7 @@ export default function Galeria({setDados}) {
                         <img className="flecha-guia" src={arrow} alt="flecha-circular-baixo" />
                     </article>
                     <div className="images">
-                        {imagensLista}
+                        {load === false ? (<img className="load" src={Load} alt="loading" />):(imagensLista)}
                     </div>
                 </div>
                 <div className="caixa-espaço"></div>
